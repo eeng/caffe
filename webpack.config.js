a767d1c378b3b9b70fd175a18497b5ac087535e1
Emmanuel Nicolau <emmanicolau@gmail.com>
2019-12-28 19:35:02 -0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = env => {
+  isEnvProduction = env === 'production';
+  isEnvDevelopment = !isEnvProduction;
+
+  var devtool = isEnvDevelopment
+    ? "eval-source-map"
+    : "nosources-source-map"
+
+  return {
+    mode: isEnvProduction ? 'production' : 'development',
+    entry: './src/index.tsx',
+    module: {
+      rules: [
+        {
+          test: /\.ts(x?)$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+        {
+          enforce: "pre",
+          test: /\.js$/,
+          loader: 'source-map-loader'
+        },
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+      ]
+    },
+    output: {
+      filename: isEnvProduction ? '[name].[contenthash].bundle.js' : '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+      publicPath: '/',
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({ title: 'Caffe' }),
+    ],
+    resolve: {
+      extensions: ['.js', '.ts', '.tsx'],
+    },
+    devtool: devtool,
+    devServer: {
+      contentBase: './dist',
+    },
+    optimization: {
+      moduleIds: 'hashed',
+      runtimeChunk: 'single',
+      splitChunks: {
+        cacheGroups: {
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name: 'vendors',
+            chunks: 'all',
+          },
+        },
+      },
+    },
+  };
+}
