a8daf6b274f3131adcc5effb23e4cfc6e345a949
Emmanuel Nicolau <emmanicolau@gmail.com>
2019-12-28 23:42:18 -0300

    Add jest

---
+++ src/components/App.test.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+// import { render } from "@testing-library/react";
+import App from "./App";
+
+test("renders learn react link", () => {
+  expect(1 + 1).toBe(2);
+  // const { getByText } = render(<App />);
+  // const linkElement = getByText(/learn react/i);
+  // expect(linkElement).toBeInTheDocument();
+});
