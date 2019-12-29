e62577f57cbc9705dbb8dfa35e64803c9153ded2
Emmanuel Nicolau <emmanicolau@gmail.com>
2019-12-29 00:06:40 -0300

    Add react testing library

--- src/components/App.test.tsx
+++ src/components/App.test.tsx
@@ -1,10 +1,9 @@
 import React from "react";
-// import { render } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
-  expect(1 + 1).toBe(2);
-  // const { getByText } = render(<App />);
-  // const linkElement = getByText(/learn react/i);
-  // expect(linkElement).toBeInTheDocument();
+test("renders ok", () => {
+  const { getByText } = render(<App />);
+  const element = getByText(/environment/i);
+  expect(element).toBeInTheDocument();
 });
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import styles from "./App.module.css";
 
 const App: React.FC = () => {
-  return <div className={styles.app}>env: {process.env.NODE_ENV}</div>;
+  return <div className={styles.app}>environment: {process.env.NODE_ENV}</div>;
 };
 
 export default App;
