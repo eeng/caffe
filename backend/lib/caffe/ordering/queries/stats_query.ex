0eecadc0772045ae0fac35407ed7792032b55c5f
Emmanuel Nicolau <emmanicolau@gmail.com>
2020-01-28 12:32:20 -0300

    Fix fragile test

--- backend/lib/caffe/ordering/queries/stats_query.ex
+++ backend/lib/caffe/ordering/queries/stats_query.ex
@@ -9,9 +9,10 @@ defmodule Caffe.Ordering.Queries.StatsQuery do
         where: o.state != "cancelled",
         select: %{order_count: count(o.id), amount_earned: sum(o.order_amount) |> coalesce(0)}
 
-    filters
-    |> Enum.reduce(query, fn
-      {:since, since}, query -> query |> where([o], o.order_date >= ^since)
-    end)
+    Enum.reduce(filters, query, &filter_by/2)
+  end
+
+  defp filter_by({:since, since}, query) do
+    query |> where([o], o.order_date >= ^since)
   end
 end
